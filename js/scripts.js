9e8c8404d3746b0d25fae20821a6322a0f4a414b
David Rolfs and Halle Tempest <david@>
2017-04-26 14:57:34 -0700

    have users input display on page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  $("form#info").submit(function(){
+  $("form#info").submit(function(event){
+    event.preventDefault()
     var firstName=$("#firstName").val();
     var lastName=$("#lastName").val();
     var description=$("#description").val();
@@ -7,7 +8,13 @@ $(document).ready(function(){
     var start=$("#start").val();
     var end=$("#end").val();
 
-    
+    $("#show").show();
+    $(".firstName").text(firstName);
+    $(".lastName").text(lastName);
+    $(".description").text(description);
+    $(".date").text(date);
+    $(".start").text(start);
+    $(".end").text(end);
 
   });
 });
