216d6dffa61be7feaa6300f1d5fb48bef517f95b
David Rolfs and Halle Tempest <david@>
2017-04-26 14:49:52 -0700

    store user input in variable using jQuery

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("form#info").submit(function(){
+    var firstName=$("#firstName").val();
+    var lastName=$("#lastName").val();
+    var description=$("#description").val();
+    var date=$("#date").val();
+    var start=$("#start").val();
+    var end=$("#end").val();
+
+    
+
+  });
+});
